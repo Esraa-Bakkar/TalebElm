4461ce7144e5aeabf2a6d1a3c7d5b65ea3188ba5
Esraa Bakkar <esraabakkar959@gmail.com>
2026-08-24 20:18:32 +0300

    Feat : Create Lesson Entity

--- src/TalebElm.Domain/Entities/Lesson.cs
+++ src/TalebElm.Domain/Entities/Lesson.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace TalebElm.Domain.Entities
 {
-    public class Lesson 
+    public class Lesson :BaseEntity
     { 
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public int Order { get; set; }
+
+        public Guid ModuleId { get; set; }
 
     }
 }
