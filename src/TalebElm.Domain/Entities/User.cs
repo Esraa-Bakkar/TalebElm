b71761fe9ea365fc89dbc6002461ed6b31fdf061
Esraa Bakkar <esraabakkar959@gmail.com>
2026-08-29 12:20:59 +0300

    Feat :Create LessonTypeEnum

--- src/TalebElm.Domain/Entities/User.cs
+++ src/TalebElm.Domain/Entities/User.cs
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 ﻿using System;
 
 namespace TalebElm.Domain.Entities;
+=======
+﻿namespace TalebElm.Domain.Entities;
+>>>>>>> 5c6b99ebb1db6d9729c8bbe1cf902451b4c539a4
 
 public class User : BaseEntity
 {
